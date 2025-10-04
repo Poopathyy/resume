8c5764b6ea910b54428edb280770c772dddd6b06
Poopathy <poopathyswaq@gmail.com>
2025-10-04 14:00:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ st.markdown(
 # -------------------------------
 col1, col2 = st.columns([1, 3])
 with col1:
-    st.image("profilePic.jpeg", width=200)
+    st.image("profilePic.jpeg", width=300)
 
 with col2:
     st.title("POOPATHY A/L RAVI")
